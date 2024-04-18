38a47004d9513b28db203e12373f437628c3b67e
Konrad1991 <konrad_kraemer@yahoo.de>
2024-04-18 14:54:10 +0200

    small update

--- tests/testCpp.cpp
+++ tests/testCpp.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <type_traits>
+#include <vector>
+
+template <typename L, typename R> class MyClass {
+public:
+  MyClass(const L l, const R r) : l(l), r(r) { res = l + r; }
+
+private:
+  L l;
+  R r;
+  std::common_type_t<L, R> res;
+};
 
 int main(int argc, char *argv[]) {
   std::cout << "test" << std::endl;
+
+  std::vector<double> v{3};
+
+  MyClass mc(1, 2);
+
   return 0;
 }
